246452199ea1a9fec1439d9011f0cca79ee51787
cedricmorreau <cedricmorreau@gmail.com>
2024-05-28 11:44:11 +0200

    Update/ Slidethree > height bug

--- components/SlideThree.tsx
+++ components/SlideThree.tsx
@@ -26,7 +26,7 @@ export default function SlideThree(props: any) {
   const isButtonDisabled = !tandcChecked;
   return (
     <>
-      <div className="md:flex  mb-12 md:px-5 px-0 lg:gap-10 gap-5 xl:px-0 lg:justify-between max-w-6xl mx-auto">
+      <div className="md:flex mb-12 md:px-5 px-0 lg:gap-10 gap-5 xl:px-0 lg:justify-between max-w-6xl mx-auto">
         <div className="hidden md:block md:w-5/12 md:max-w-[25rem]">
           <Warning />
         </div>
