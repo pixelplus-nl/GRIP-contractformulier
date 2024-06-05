5bb95c7e44663e7f8899611da46560f8f121b14f
Noah Wijkel <noah@pixelplus.nl>
2024-06-05 16:28:05 +0200

    Fix up error texts

--- components/ErrorModal.tsx
+++ components/ErrorModal.tsx
@@ -16,12 +16,12 @@ export default function ErrorModal(props: any) {
         whileInView={{ opacity: 1 }}
         transition={{ delay: 0.5 }}
         className="relative z-50 bg-white p-10 rounded-2xl">
-        <h1 className="text-4xl font-bold mb-5 pr-10">{props.title}</h1>
+        <h1 className="text-4xl font-bold mb-5 pr-10">{props.errorModal.title}</h1>
 
-        {Array.isArray(props.body) ? (
-          <ul className={"list-disc ml-5 max-w-2xl"}>{props.body}</ul>
+        {Array.isArray(props.errorModal.body) ? (
+          <ul className={"list-disc ml-5 max-w-2xl"}>{props.errorModal.body}</ul>
         ) : (
-          <p className={"max-w-2xl"}>{props.body}</p>
+          <p className={"max-w-2xl"}>{props.errorModal.body}</p>
         )}
 
         <div className={`flex gap-3 mt-5 ${props.languageButtons}`}>
