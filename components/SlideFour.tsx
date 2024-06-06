aa2e18ab042a3fdd5fd741279d1a06ee85d1c074
Noah Wijkel <noah@pixelplus.nl>
2024-06-06 09:14:40 +0200

    Add barebones onClose callback for error modal

--- components/SlideFour.tsx
+++ components/SlideFour.tsx
@@ -85,6 +85,10 @@ export default function SlideFour(props: any) {
       props.setErrorModal({
         title: errorModal("titleSuccess"),
         body: errorModal("bodySuccess"),
+        onClose: () => {
+          // TODO: Redirect to some other page (e.g. a success page)
+          //router.push("/success");
+        }
       });
     }
   };
