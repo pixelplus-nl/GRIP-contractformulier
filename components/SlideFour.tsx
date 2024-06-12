315235a951671551f6906c1f82b79118e02dda0b
cedricmorreau <cedricmorreau@gmail.com>
2024-06-12 16:54:18 +0200

    Update/ slide four padding bug

--- components/SlideFour.tsx
+++ components/SlideFour.tsx
@@ -218,7 +218,7 @@ export default function SlideFour(props: any) {
                     />
                     <label
                       htmlFor="middle-name"
-                      className="absolute  text-gray-400  duration-300 transform -translate-y-4 scale-75 top-[2px] z-10 origin-[0] bg-white  px-2 peer-focus:px-2 peer-focus:text-gray-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-[2px] peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+                      className="absolute  text-gray-400  duration-300 transform -translate-y-4 scale-75 top-[2px] z-10 origin-[0] bg-white  pl-2 peer-focus:px-2 peer-focus:text-gray-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-[2px] peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
                       {t("middleName")}
                     </label>
                   </div>
